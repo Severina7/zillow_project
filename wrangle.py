79a3cd42bd44ca5997467b1f357dbca0c384ba3a
Arsene Severin Boundaone <arsene.boundaone@gmail.com>
2022-06-14 22:08:16 -0500

    updating files

--- wrangle.py
+++ wrangle.py
@@ -41,38 +41,38 @@ def get_zillow_data():
     return df
 
 
-# def get_local_zillow():
-#     '''
-#     get_local_zillow reads in telco data from Codeup database, writes data to
-#     a csv file if a local file does not exist, and returns a DF.
-#     '''
-#     if os.path.isfile('properties_2017.csv'):
+def get_local_zillow():
+    '''
+    get_local_zillow reads in telco data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a DF.
+    '''
+    if os.path.isfile('properties_2017.csv'):
         
-#         # If csv file exists read in data from csv file.
-#         df = pd.read_csv('properties_2017.csv', index_col=0)
+        # If csv file exists read in data from csv file.
+        df = pd.read_csv('properties_2017.csv', index_col=0)
         
-#     else:
+    else:
         
-#         # Read fresh data from db into a DataFrame
-#         df = get_zillow_data()
+        # Read fresh data from db into a DataFrame
+        df = get_zillow_data()
         
-#         # Cache data
-#         df.to_csv('properties_2017.csv')
+        # Cache data
+        df.to_csv('properties_2017.csv')
         
-#     return df
+    return df
 
-# def split_zillow(df):
-#     '''
-#     Takes in a DataFrame and returns train, validate, and test DataFrames.
-#     '''
-#     # splits df into train_validate and test using train_test_split()
-#     train_validate, test = train_test_split(df, test_size=.2, random_state=175)
+def split_zillow(df):
+    '''
+    Takes in a DataFrame and returns train, validate, and test DataFrames.
+    '''
+    # splits df into train_validate and test using train_test_split()
+    train_validate, test = train_test_split(df, test_size=.2, random_state=175)
     
-#     # splits train_validate into train and validate using train_test_split() stratifying on species to get an even mix of each species
-#     train, validate = train_test_split(train_validate, 
-#                                        test_size=.3, 
-#                                        random_state=175)
-#     return train, validate, test
+    # splits train_validate into train and validate using train_test_split() stratifying on species to get an even mix of each species
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=175)
+    return train, validate, test
 
 
 ################# Cleaning and splitting the Zillow Data ######################
@@ -80,24 +80,23 @@ def get_zillow_data():
 def wrangle_zillow():
     '''Cleans, and splits Zillow Data for exploration, once acquired'''
 
-# Compiling the code for wrangling data
+# # Compiling the code for wrangling data
 
-    sql_query = '''
-                SELECT 
-                       bedroomcnt,
-                       bathroomcnt,
-                       calculatedfinishedsquarefeet,
-                       taxvaluedollarcnt,
-                       propertylandusetypeid,
-                       transactiondate
-                FROM properties_2017
-                JOIN predictions_2017 USING(parcelid)
-                JOIN propertylandusetype USING(propertylandusetypeid)
-                WHERE propertylandusetypeid = '261'
-                '''
+#     sql_query = '''
+#                 SELECT 
+#                        bedroomcnt,
+#                        bathroomcnt,
+#                        calculatedfinishedsquarefeet,
+#                        taxvaluedollarcnt,
+#                        propertylandusetypeid,
+#                        transactiondate
+#                 FROM properties_2017
+#                 JOIN predictions_2017 USING(parcelid)
+#                 JOIN propertylandusetype USING(propertylandusetypeid)
+#                 WHERE propertylandusetypeid = '261'
+#                 '''
     
     # Reading in the DataFrame from Codeup db.
-    properties_2017 = pd.read_sql(sql_query, get_connection('zillow'))
     '''
     get_local_zillow reads in telco data from Codeup database, writes data to
     a csv file if a local file does not exist, and returns a df.
@@ -106,8 +105,6 @@ def wrangle_zillow():
         
         # If csv file exists read in data from csv file.
         properties_2017 = pd.read_csv('properties_2017.csv', index_col=0)
-        
-        return properties_2017
         
     else:
         
@@ -143,9 +140,9 @@ def wrangle_zillow():
 
     # splits df into train_validate and test using train_test_split()
     train_validate, test = train_test_split(houses, test_size=.2, random_state=175)
-    
+
     # splits train_validate into train and validate using train_test_split() stratifying on species to get an even mix of each species
     train, validate = train_test_split(train_validate, 
-                                       test_size=.3, 
-                                       random_state=175)
+                                        test_size=.3, 
+                                        random_state=175)
     return train, validate, test
